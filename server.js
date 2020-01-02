4b4593c52726d1f627b2a805c629eb2c15687f84
Reshma9102 <mnvreshma@gmail.com>
2020-01-02 11:45:15 +0530

    final checkin

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const paath=require('path')
+const app = express()
+const port = process.env.PORT||3000
+app.use(express.static(__dirname+'/dist/ngproject'));
+app.get('/', (req, res) => res.send(path.join(__dirname+'/dist/ngproject/index.html')))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
